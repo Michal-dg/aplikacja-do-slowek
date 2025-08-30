14047eae1ecea5fc42b27efa7153f7d7163e09d0
Michal-d <black.solo.fan@gmail.com>
2025-08-30 21:08:50 +0200

    Pierwsza wersja aplikacji

---
+++ slowka_baza_en.js
@@ -0,0 +1,7 @@
+const initialWordList = [
+    { english: 'the', polish: '(określony)', example_en: 'The car is red.', example_pl: 'Samochód jest czerwony.' },
+    { english: 'be', polish: 'być', example_en: 'I want to be a doctor.', example_pl: 'Chcę być lekarzem.' },
+    { english: 'to', polish: 'do, żeby', example_en: 'I go to school.', example_pl: 'Idę do szkoły.' },
+    { english: 'of', polish: 'z (kogoś/czegoś)', example_en: 'A cup of tea.', example_pl: 'Filiżanka herbaty.' },
+    { english: 'and', polish: 'i', example_en: 'Bread and butter.', example_pl: 'Chleb i masło.' }
+];
